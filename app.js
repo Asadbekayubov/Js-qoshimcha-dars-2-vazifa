8841c603af7b97cc526901f8476f1fd824cd0eb1
Asadulloh <ayubovasadbek33@gmail.com>
2023-07-28 08:25:14 +0500

    commited

---
+++ app.js
@@ -0,0 +1,59 @@
+// 1 chi Masala
+// let yosh = Number(prompt("Yoshingizni kiriting:"))
+// if(yosh>50){
+//     console.log("Siz Kattasiz");
+// }else{
+//     console.log("Siz Yoshsiz");
+// }
+
+// ------------------------------------------------------
+
+// 2 chi Masala
+// let somsaNarxi = Number(prompt("Somsa Narxini kiriting:"))
+// if(somsaNarxi > 3000){
+//     console.log("Qimmat");
+// }else{
+//     console.log("Arzon");
+// }
+
+// ------------------------------------------------------
+
+// 3 chi Masala
+// let userParol = prompt("Parolni kiriting:")
+// let parol = "sarimsoqpiyoz"
+// if(parol == userParol){
+//     console.log("Assalomu Alekum");
+// }
+
+// ------------------------------------------------------
+
+// 4 chi Masala
+// let userParol = prompt("Parolni kiriting:")
+// let parol = "sarimsoqpiyoz"
+// if(parol == userParol){
+//     console.log("Assalomu Alekum");
+// }else{
+//     console.log("Xato!");
+// }
+
+// ------------------------------------------------------
+
+// 5 chi Masala
+// let userAge = Number(prompt("Yoshingizni kiriting:"))
+// if(userAge > 0 && userAge < 30){
+//     console.log("Yoshsiz");
+// }else if(userAge >=30 && userAge < 60){
+//     console.log("Kattasiz");
+// }else{
+//     console.log("Qarisiz");
+// }
+
+// ------------------------------------------------------
+
+// 6 chi Masala
+// let userName = prompt("Ismingizni kiriting:")
+// if (userName == "Ali"){
+//     console.log("Salom Og'a");
+// }else if(userName == "Ahmad"){
+//     console.log("Korinmayapsan dostim , nima qilyapsan?");
+// }
